abe44c1deaffb0f53fc730659fe797018a038773
kokoseda <kokoseda22@yahoo.com>
2018-11-06 13:33:43 +0200

    Add files via upload

---
+++ bot (3).js
@@ -0,0 +1,64 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("IM READY.!");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("492681261043941376")
+setInterval(function() {
+channel.send(` 0823t7cawfawfa 1`);
+channel.send(` 0823t7acawcawr 2`);
+channel.send(` 0823t7dcawcafr 3`);
+channel.send(` 0823t7214312fs 4`);
+channel.send(` 0823t75126ffaw 5`);
+channel.send(` 0823t7xawxcwaf 6`);
+channel.send(` 0823t7awfwafwa 7`);
+channel.send(` 0823t7dawxcacw 8`);
+channel.send(` 0823t7gwqdsaxc 9`);
+channel.send(` 0823t7cqwfqgg 10`);
+channel.send(` 0823t7g21fg1g 11`);
+channel.send(` 0823t7x215125 12`);
+channel.send(` 0823t7cqf214f 13`);
+channel.send(` 0823t7gwagfva 14`);
+channel.send(` 0823t76171253 15`);
+channel.send(` 0823t7cacaws2 16`);
+channel.send(` 0823t7cac2q23 17`);
+channel.send(` 0823t7vawh124 18`);
+channel.send(` 0823t7cacfgg2 19`);
+channel.send(` 0823t7hh21h21 20`);
+channel.send(` 0823t7xccwcw2 21`);
+channel.send(` 0823t7516fgsa 22`);
+channel.send(` 0823t7fwacawc 23`);
+channel.send(` 0823t79299rff 24`);
+channel.send(` 0823t7mcwoawd 25`);
+channel.send(` 0823t7lwlk2op 26`);
+channel.send(` 0823t70492010 27`);
+channel.send(` 0823t7xawfowf 28`);
+channel.send(` 0823t749210kf 29`);
+channel.send(` 0823t7gkloqws 30`);
+channel.send(` 0823t7201o43s 31`);
+channel.send(` 0823t7flawopf 32`);
+channel.send(` 0823t7awklawk 33`);
+channel.send(` 0823t7ikgwaio 34`);
+channel.send(` 0823t7fasx213 35`);
+channel.send(` 0823t72151255 36`);
+channel.send(` 0823t76216214 37`);
+channel.send(` 0823t76126612 38`);
+channel.send(` 0823t7xwqx421 39`);
+channel.send(` 0823t7hawhawf 40`);
+channel.send(` 0823t7hwahwad 41`);
+channel.send(` 0823t7gawhawh 42`);
+channel.send(` 0823t7x21x12r 43`);
+channel.send(` 0823t7hawhawh 44`);
+channel.send(` 0823t7ghwahwa 45`);
+channel.send(` 0823t72151252 46`);
+channel.send(` 0823t75125122 47`);
+channel.send(` 0823t71251251 48`);
+channel.send(` 0823t75125125 49`);
+channel.send(` 0823t7gawgaww 50`);
+
+}, 1500)
+})
+ 
+ 
+client.login("NTA4MjQxNTEwMjA3MDYyMDE3.DsCfrw.2pYvm59u8_fSohgOxl34vYIqXug");
